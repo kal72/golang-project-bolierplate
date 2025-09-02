a9313037e8788601b2be15a237cdcfedccac3d07
krisadi <>
2025-09-02 11:18:02 +0700

    [ADD] breaker : circuit breaker, retry and fallback

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,48 +2,66 @@ package config
 
 import (
 	"log"
+	"time"
 
 	"github.com/spf13/viper"
 )
 
+type AppConfig struct {
+	Name string
+	Host string
+	Port int
+}
+
+type LogConfig struct {
+	Path   string
+	Stdout bool
+}
+
+type RedisConfig struct {
+	Username string
+	Password string
+	Host     string
+	Port     int
+	DB       int
+	Pool     struct {
+		Idle    int
+		Max     int
+		Timeout int
+	}
+}
+
+type DatabaseConfig struct {
+	Username string
+	Password string
+	Host     string
+	Port     int
+	Name     string
+	Pool     struct {
+		Idle     int
+		Max      int
+		Lifetime int
+	}
+}
+
+type JwtConfig struct {
+	Secret  string
+	Expired int //minutes
+}
+
+type CircuitBreakerConfig struct {
+	FailureThreshold float64
+	MinRequests      int
+	Timeout          time.Duration
+	MaxHalfOpenReq   int
+}
+
 type Config struct {
-	App struct {
-		Name string
-		Host string
-		Port int
-	}
-	Log struct {
-		Path   string
-		Stdout bool
-	}
-	Database struct {
-		Username string
-		Password string
-		Host     string
-		Port     int
-		Name     string
-		Pool     struct {
-			Idle     int
-			Max      int
-			Lifetime int
-		}
-	}
-	Redis struct {
-		Username string
-		Password string
-		Host     string
-		Port     int
-		DB       int
-		Pool     struct {
-			Idle    int
-			Max     int
-			Timeout int
-		}
-	}
-	Jwt struct {
-		Secret  string
-		Expired int //minutes
-	}
+	App      AppConfig
+	Log      LogConfig
+	Database DatabaseConfig
+	Redis    RedisConfig
+	Jwt      JwtConfig
 }
 
 func NewConfig() *Config {
